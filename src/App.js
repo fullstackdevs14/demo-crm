5675a35aa75e47eeba7e33429461bcea8463db4c
fullstackdevs14 <fullstackdevs@outlook.com>
2020-05-13 11:41:49 -0700

    add routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
 
-function App() {
+import history from './core/history';
+
+import Login from './Login';
+import Register from './Register';
+import Portal from './Portal';
+
+const App = () => {
   return (
-    <div className="App">
-      Blank
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/">
+          <Portal />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
